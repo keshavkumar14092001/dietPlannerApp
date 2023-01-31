2de85434ffe960b2db6e1c50b091f5a2c71694f1
Keshav <keshavkumar14092001@gmail.com>
2023-01-31 14:21:47 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from config import db, SECRET_KEY
+from os import environ, path, getcwd
+from dotenv import load_dotenv
+
+load_dotenv(path.join(getcwd(), '.env'))
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DB_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = False
+    app.secret_key = SECRET_KEY
+    db.init_app(app)
+    print("DB Initialized Sucessfully")
+
+    CORS(app)
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
+        return app
+
+
+
+if __name__ == '__main__':
+    app = create_app()
