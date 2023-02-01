0cb04da891368004a40f530547de25a7d9fd375e
Ritesh12u <roni.riteshghosh@gmail.com>
2023-02-01 20:27:04 +0530

    Final Changes

--- models/diettype.py
+++ models/diettype.py
@@ -3,7 +3,6 @@ from config import db
 class DietType(db.Model):
     __tablename__='diet_type'
     id = db.Column(db.Integer, primary_key=True)
-    foodtype = db.Column(db.String(200), nullable =False)
-    foodname = db.Column(db.String(200), nullable =False)
-    calorie = db.Column(db.String(200), nullable =False)
+    day = db.Column(db.String(200), nullable =False)
+    calorie = db.Column(db.Integer, nullable =False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
